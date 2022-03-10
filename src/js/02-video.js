6ef3c94b2580677910c8da5adb56ba4b7560637d
BogdanV <zarazailo@gmail.com>
2022-03-10 23:12:45 +0200

    ff

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -29,24 +29,3 @@ player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(fun
 
 
 
-// import throttle from "lodash.throttle";
-
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-// let playerSecond;
-// const startVideoSecond = localStorage.getItem('videoplayer-current-time') ?? 0;
-
-// player.setCurrentTime(startVideoSecond)
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.on('timeupdate', function (data) {
-//     playerSecond = data.seconds;
-//     throttle(localStorage.setItem('videoplayer-current-time', playerSecond), 1000)
-//   });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
