e5cc87da36cccb9ec716c009c2281693946d5257
Charger-69 <35033759+Charger-69@users.noreply.github.com>
2022-03-10 21:13:22 +0000

    Deploying to gh-pages from @ Charger-69/goit-js-hw-08@6ef3c94b2580677910c8da5adb56ba4b7560637d 🚀

--- 02-video.dc5163cc.js
+++ 02-video.dc5163cc.js
@@ -7,4 +7,4 @@ var t=arguments[3],n="Expected a function",e=NaN,i="[object Symbol]",r=/^\s+|\s+
 },{}],"iiAF":[function(require,module,exports) {
 "use strict";var e=r(require("@vimeo/player")),t=r(require("lodash.throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const o=document.querySelector("iframe#vimeo-player"),n=new e.default(o),l=function(e){let t=e.seconds;console.log(t),localStorage.setItem("videoplayer-current-time",t)};n.on("timeupdate",(0,t.default)(l,1e3)),n.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(e){}).catch(function(e){e.name});
 },{"@vimeo/player":"zU8P","lodash.throttle":"FNyO"}]},{},["iiAF"], null)
-//# sourceMappingURL=/parcel-project-template/02-video.0e22717d.js.map
+//# sourceMappingURL=/parcel-project-template/02-video.dc5163cc.js.map
